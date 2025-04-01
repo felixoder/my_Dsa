a6e7c36f6f7fd5719e09b70c03b9fdbf223f8fb7
felixoder <debayanghosh408@gmail.com>
2025-04-01 10:49:47 +0530

    added rest of medium

--- src/app/(routes)/array/medium/page.tsx
+++ src/app/(routes)/array/medium/page.tsx
@@ -31,7 +31,47 @@ export default function ArrayMediumPage() {
           5. Stock Buy and Sell
           <span className="text-orange-400">Medium</span>
         </Link>
+        <div className="flex justify-center items-center flex-col gap-5">
+        <Link href="https://leetcode.com/problems/rearrange-array-elements-by-sign/description/">
+          6. Rearranging array by sign
+          <span className="text-green-400">Medium</span>
+        </Link>
+        <Link href="https://leetcode.com/problems/next-permutation/">
+          7. Next Permutation
+          <span className="text-orange-400">Medium</span>
+        </Link>
+        <Link href="https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=leaders-in-an-array">
+          8. Leaders in an array
+          <span className="text-orange-400">Medium</span>
+        </Link>
+        <Link href="https://leetcode.com/problems/longest-consecutive-sequence/editorial/">
+          9. Longest concecutive sequence [Premium]
+          <span className="text-orange-400">Medium</span>
+        </Link>
+        <Link href="https://www.naukri.com/code360/problems/longest-successive-elements_6811740?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse">
+          10. Longest concecutive sequence  [Code studio]
+        </Link>
+        <Link href="https://leetcode.com/problems/set-matrix-zeroes/">
+          11. Set matrix zero
+          <span className="text-orange-400">Medium</span>
+        </Link>
+        <Link href="https://leetcode.com/problems/spiral-matrix/description/">
+          12. Spiral matrix
+          <span className="text-orange-400">Medium</span>
+        </Link>
+        <Link href="https://www.naukri.com/code360/problems/longest-successive-elements_6811740?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse">
+          13. Longest concecutive sequence  [Code studio]
+        </Link>
+        <Link href="https://leetcode.com/problems/set-matrix-zeroes/">
+          14. Set matrix zero
+          <span className="text-orange-400">Medium</span>
+        </Link>
+        <Link href="https://leetcode.com/problems/subarray-sum-equals-k/">
+          15. Subarray Sum equals K
+          <span className="text-orange-400">Medium</span>
+        </Link>
       </div>
+    </div>
     </div>
   );
 }
